92d29d90ed288e7f33b7dc550b32f1aeab8e8f4c
geo318 <geo.lomidze@gmail.com>
2023-05-31 17:38:34 +0400

    add painting mongo model

---
+++ src/models/painting.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const paintingSchema = new mongoose.Schema(
+  {
+    name: Schema.Types.String,
+    size: Schema.Types.String,
+    description: Schema.Types.String,
+    year: Schema.Types.Number,
+    images: [
+      {
+        image: Schema.Types.String,
+        thumbnail: Schema.Types.String,
+      },
+    ],
+  },
+  { versionKey: false, timestamps: true }
+);
+
+const Painting = mongoose.model("painting", paintingSchema);
+
+export default Painting;
